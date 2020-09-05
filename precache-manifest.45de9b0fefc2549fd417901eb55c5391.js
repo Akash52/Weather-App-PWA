2d7197510b7a053b53408764a9861534a833a963
Akash52 <ac8572611@gmail.com>
2020-09-05 01:36:02 -0700

    Updates

---
+++ precache-manifest.45de9b0fefc2549fd417901eb55c5391.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e4cd2bec6a3435dd96aa71df952b44c",
+    "url": "/Weather-App-PWA/index.html"
+  },
+  {
+    "revision": "02b8456d45d56d3b00e9",
+    "url": "/Weather-App-PWA/static/css/main.fa22dafe.chunk.css"
+  },
+  {
+    "revision": "3eb2af611f84783caa3a",
+    "url": "/Weather-App-PWA/static/js/2.63334aaa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Weather-App-PWA/static/js/2.63334aaa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02b8456d45d56d3b00e9",
+    "url": "/Weather-App-PWA/static/js/main.5d0f2f77.chunk.js"
+  },
+  {
+    "revision": "369abb3b198cb5b3c701",
+    "url": "/Weather-App-PWA/static/js/runtime-main.20a2ce6a.js"
+  }
+]);
